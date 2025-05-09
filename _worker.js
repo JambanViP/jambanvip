5afd54b3b533035f9b7a09fcd61cf7483300ed8b
JambanViP <141326378+JambanViP@users.noreply.github.com>
2025-05-09 12:54:17 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "akunbuatjamban1.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "jambanvip"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiKey = "Gy_mXxAn8xFpLBv-8uzHEMYorNZj_GFs1hxQ4tbP"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
